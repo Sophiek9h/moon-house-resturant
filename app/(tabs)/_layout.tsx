9a982fdff278946b78a5803c7f1833afefadae5f
Sophie kalagbor <sophiek9h@gmail.com>
2025-09-09 21:21:12 +0100

    setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,30 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import React from 'react'
+import { Tabs } from 'expo-router'
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+// const TabIcon = ({focused, icon, title}: {focused: boolean, icon: any, title: string}) => (
+//     <View className='flex-1 mt-3 flex flex-col justify-center items-center'>
+//         <Image source={icons} tintColor={focused? '#0061ff' : '#666876'} resizeMode='contain' className='size-6'/>
+//         <Text className={`text-xs mt-1 w-full text-center ${focused? 'text-primary-300 font-rubik-medium' : 'text-black-200 font-rubik'}`}>{title}</Text>
+//     </View>
+// )
 
+const _layout = () => {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <Tabs>
+        <Tabs.Screen
+            name="index"
+            options={{
+                title: "Home",
+                headerShown: false,
+                // tabBarIcon: () => (
+                //     // <TabIcon />
+                // )
+            }}
+
+        />
     </Tabs>
-  );
+  )
 }
+
+export default _layout
